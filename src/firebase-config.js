a0983759a53f85a35a90af279b10639f648431d0
ishantsehrawat <ishantsehrawat75@gmail.com>
2022-12-10 16:44:07 +0530

    firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCskvv7rKBHyWIXgz0J8-Jv4k6Tv0w3FIE",
+  authDomain: "ikan-47608.firebaseapp.com",
+  projectId: "ikan-47608",
+  storageBucket: "ikan-47608.appspot.com",
+  messagingSenderId: "996677061517",
+  appId: "1:996677061517:web:e375c92f61fde576108bc4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
